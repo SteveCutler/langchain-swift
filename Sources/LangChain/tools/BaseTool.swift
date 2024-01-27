bcfc55ed1740793ac95b76703e61942f791f0c39
SteveCutler <Stephen.c.cutler@gmail.com>
2024-01-26 22:59:13 -0500

    Update BaseTool.swift
    made public

--- Sources/LangChain/tools/BaseTool.swift
+++ Sources/LangChain/tools/BaseTool.swift
@@ -17,51 +17,30 @@ public protocol Tool {
     func _run(args: String) async throws -> String
 }
 open class BaseTool: NSObject, Tool {
-    static let TOOL_REQ_ID = "tool_req_id"
-    static let TOOL_COST_KEY = "cost"
-    static let TOOL_NAME_KEY = "tool_name"
-    let callbacks: [BaseCallbackHandler]
-    init(callbacks: [BaseCallbackHandler] = []) {
-        var cbs: [BaseCallbackHandler] = callbacks
-        if Env.addTraceCallbak() && !cbs.contains(where: { item in item is TraceCallbackHandler}) {
-            cbs.append(TraceCallbackHandler())
-        }
-//        assert(cbs.count == 1)
-        self.callbacks = cbs
+    public static let TOOL_REQ_ID = "tool_req_id"
+    public static let TOOL_COST_KEY = "cost"
+    public static let TOOL_NAME_KEY = "tool_name"
+
+    public let callbacks: [BaseCallbackHandler]
+
+    public init(callbacks: [BaseCallbackHandler] = []) {
+        self.callbacks = callbacks
+        super.init()
     }
-    func callStart(tool: BaseTool, input: String, reqId: String) {
-        do {
-            for callback in callbacks {
-                try callback.on_tool_start(tool: tool, input: input, metadata: [BaseTool.TOOL_REQ_ID: reqId, BaseTool.TOOL_NAME_KEY: tool.name()])
-            }
-        } catch {
-            
-        }
-    }
-    
-    func callEnd(tool: BaseTool, output: String, reqId: String, cost: Double) {
-        do {
-            for callback in callbacks {
-                try callback.on_tool_end(tool: tool, output: output, metadata: [BaseTool.TOOL_REQ_ID: reqId, BaseTool.TOOL_COST_KEY: "\(cost)", BaseTool.TOOL_NAME_KEY: tool.name()])
-            }
-        } catch {
-            
-        }
-    }
-    
+
     open func name() -> String {
-        ""
+        fatalError("Subclasses need to implement the `name()` method.")
     }
-    
+
     open func description() -> String {
-        ""
+        fatalError("Subclasses need to implement the `description()` method.")
     }
-    
+
     open func _run(args: String) async throws -> String {
-        ""
+        fatalError("Subclasses need to implement the `_run(args:)` method.")
     }
-    
-    open func run(args: String) async throws -> String {
+
+    public func run(args: String) async throws -> String {
         let reqId = UUID().uuidString
         var cost = 0.0
         let now = Date.now.timeIntervalSince1970
@@ -71,5 +50,5 @@ open class BaseTool: NSObject, Tool {
         callEnd(tool: self, output: result, reqId: reqId, cost: cost)
         return result
     }
-    
+
 }
