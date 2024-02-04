38b51e71fe4b81366b2e5dccc820a9bd8d321d06
SteveCutler <Stephen.c.cutler@gmail.com>
2024-02-04 14:12:33 -0500

    Update OpenAI.swift
    changed from http to URL session

--- Sources/LangChain/llms/OpenAI.swift
+++ Sources/LangChain/llms/OpenAI.swift
@@ -33,9 +33,16 @@ public class OpenAI: LLM {
                 // it's important to shutdown the httpClient after all requests are done, even if one failed. See: https://github.com/swift-server/async-http-client
                 try? httpClient.syncShutdown()
             }
-            let configuration = Configuration(apiKey: apiKey, api: API(scheme: .https, host: baseUrl))
 
-            let openAIClient = OpenAIKit.Client(httpClient: httpClient, configuration: configuration)
+            
+
+            let urlSession = URLSession(configuration: .default)
+            let configuration = Configuration(apiKey: apiKey, organization: organization)
+            let openAIClient = OpenAIKit.Client(session: urlSession, configuration: configuration)
+        
+     //       let configuration = Configuration(apiKey: apiKey, api: API(scheme: .https, host: baseUrl))
+
+     //       let openAIClient = OpenAIKit.Client(httpClient: httpClient, configuration: configuration)
             
             let completion = try await openAIClient.chats.create(model: model, messages: [.user(content: text)], temperature: temperature, stops: stops)
             return LLMResult(llm_output: completion.choices.first!.message.content)
