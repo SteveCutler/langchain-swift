2fdf9a720902892488d377695b498607c7cf9ca0
SteveCutler <Stephen.c.cutler@gmail.com>
2024-02-04 14:59:22 -0500

    Update WikipediaAPIWrapper.swift

--- Sources/LangChain/utilities/wikipedia/WikipediaAPIWrapper.swift
+++ Sources/LangChain/utilities/wikipedia/WikipediaAPIWrapper.swift
@@ -10,7 +10,7 @@ import SwiftyJSON
 import NIOPosix
 import Alamofire
 
-struct WikipediaAPIWrapper {
+public struct WikipediaAPIWrapper {
   public func search(query: String) async throws -> [WikipediaPage] {
         let baseURL = "https://en.wikipedia.org/w/api.php"
         let parameters: Parameters = [
